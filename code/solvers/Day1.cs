477c603eecd1606a63721c0f0d27dd65da4f6885
Nikola <nikoladragovic@hotmail.com>
2022-12-01 23:04:58 +0100

    day01

--- code/solvers/Day1.cs
+++ code/solvers/Day1.cs
@@ -1,22 +1,45 @@
 public class Day1:Solver {
 
     /// <summary>
-    /// The input variable idk
+    /// An enumerable which represents the items of each elf
     /// </summary>
-    string inp;
+    IEnumerable<IEnumerable<Int32>> inp;
 
+    /// <summary>
+    /// Initialize the solver by supplying the path to the input
+    /// </summary>
+    /// <param name="inputFile"> Path to input file </param>
     public Day1 (string inputFile) : base(inputFile) 
     {
-        this.inp = "Hello World";
+        this.inp = System.IO.File.ReadAllText("input/main/day01")
+            .Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries)
+            .Select(elf => elf.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+                .Select(cal => Int32.Parse(cal)
+                )
+            );
     }
 
+    /// <summary>
+    /// Find the Elf carrying the most Calories. 
+    /// How many total Calories is that Elf carrying?
+    /// </summary>
     public override void SolvePartOne()
     {
-        Console.WriteLine(this.inp);
+        int result = this.inp.Select(items => items.Sum())
+            .Max();
+        Console.WriteLine(result);
     }
 
+    /// <summary>
+    /// Find the top three Elves carrying the most Calories. 
+    /// How many Calories are those Elves carrying in total?
+    /// </summary>
     public override void SolvePartTwo()
     {
-        Console.WriteLine(this.inp + " but part two");
+        int result = this.inp.Select(items => items.Sum())
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum();
+        Console.WriteLine(result);
     }
 }
